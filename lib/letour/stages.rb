67e8456dcf612e92681b889ba6cc01ee560116ab
Jennifer Haddock <jentx123@hotmail.com>
2017-08-07 23:54:09 +0000

    initial stages class

--- lib/letour/stages.rb
+++ lib/letour/stages.rb
@@ -1,3 +1,20 @@
 class LeTour::Stages
+  attr_accessor :stage, :type, :start, :finish, :distance, :date, :details
+
+  @@all = []
+
+  def intialize(:stage, :type, :start, :finish, :distance, :date, :details)
+    @stage = stage
+    @type = type
+    @start = start
+    @finish = finish
+    @distance = distance
+    @date = date
+    @details = details
+  end
+
+  def self.all
+    @@all
+  end
 
 end
