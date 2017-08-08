3b8fd9d1f6998d1afd72205b4cf7abcf5a92e746
Jennifer Haddock <jentx123@hotmail.com>
2017-08-08 01:04:21 +0000

    scraper class changes

--- lib/letour/stages.rb
+++ lib/letour/stages.rb
@@ -17,5 +17,20 @@ class LeTour::Stages
     @@all
   end
 
+  def self.new_from_site(stage_info)
+    if stage_info.css('td.etape')[0].text != '-'
+      town_array = stage_info.css('td.parcours')[0].text.split(" / ")
+
+  binding.pry
+      self.new(stage: stage_info.css('td.etape')[0].text,
+               type: stage_info.css('td.type prolouge')[0].text,
+               date: stage_info.css('td.date')[0].text,
+               start: town_array[0],
+               finish: town_array[1],
+               distance: stage_info.css('td.distance')[0].text,
+               details: stage_info.css('td.details a')[0]['href'])
+
+    end
+  end
 
 end
